f67fc5524f17f1f6c5d2a5803560d3563872d0bd
Martin Habovstiak <martin.habovstiak@gmail.com>
2019-11-02 17:38:15 +0100

    Added missing slash in install

--- src/bin/generator/install.rs
+++ src/bin/generator/install.rs
@@ -7,7 +7,7 @@ pub fn generate(instance: &PackageInstance, out: LazyCreateBuilder) -> io::Resul
     for (file_name, conf) in instance.config() {
         if let ConfType::Static { internal, .. } = &conf.conf_type {
             let dir = file_name.rfind('/').map(|pos| &file_name[..pos+1]).unwrap_or("");
-            writeln!(out, "{}/etc/{}/{} /etc/{}{}", instance.name, instance.config_sub_dir(), file_name, instance.config_sub_dir(), dir)?;
+            writeln!(out, "{}/etc/{}/{} /etc/{}/{}", instance.name, instance.config_sub_dir(), file_name, instance.config_sub_dir(), dir)?;
             if *internal {
                 writeln!(out, "{}/usr/share/{}/internal_config/{} /usr/share/{}/internal_config/{}", instance.name, instance.internal_config_sub_dir(), file_name, instance.internal_config_sub_dir(), dir)?;
             }
