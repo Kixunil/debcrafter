e7f09639a13d9163548aa11fb0f35248ef0a5b55
Martin Habovstiak <martin.habovstiak@gmail.com>
2020-03-14 11:20:40 +0100

    Run file triggers in postrm.
    Changes to files made in postrm need to be signalled to every package
    that depends on those files. Thus, trigger is called in postrm.
    
    Fixes #21

--- src/bin/generator/postrm.rs
+++ src/bin/generator/postrm.rs
@@ -5,10 +5,22 @@ use crate::codegen::{LazyCreateBuilder};
 pub fn generate(instance: &PackageInstance, out: LazyCreateBuilder) -> io::Result<()> {
     let out = out.set_header("#!/bin/bash\n\nif [ \"$1\" = purge ];\nthen\n");
     let mut out = out.finalize();
+    let mut trigger_dir = false;
     for (file_name, conf) in instance.config() {
         if let ConfType::Dynamic { .. } = &conf.conf_type {
             writeln!(out, "\trm -f /etc/{}/{}", instance.config_sub_dir(), file_name)?;
+            writeln!(out, "\tdpkg-trigger /etc/{}/{}", instance.config_sub_dir(), file_name)?;
+            if let Some(pos) = file_name.rfind('/') {
+                // We need to trigger the relevant directories in order to update other packages
+                writeln!(out, "\tdpkg-trigger /etc/{}/{}", instance.config_sub_dir(), &file_name[..pos])?;
+            } else {
+                trigger_dir = true;
+            }
         }
+    }
+
+    if trigger_dir {
+        writeln!(out, "\tdpkg-trigger /etc/{}", instance.config_sub_dir())?;
     }
 
     if let Some(out) = out.created() {
