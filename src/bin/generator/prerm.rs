2a4c66241c02d2bf7094c91518efce16177aa35e
Martin Habovstiak <martin.habovstiak@gmail.com>
2020-06-12 08:24:35 +0200

    Refactor writing alternatives to a separate fn

--- src/bin/generator/prerm.rs
+++ src/bin/generator/prerm.rs
@@ -4,10 +4,7 @@ use crate::codegen::{LazyCreateBuilder};
 use std::borrow::Cow;
 use std::collections::HashSet;
 
-pub fn generate(instance: &PackageInstance, out: LazyCreateBuilder) -> io::Result<()> {
-    let out = out.set_header("#!/bin/bash\n\nset -e\n\n");
-    let mut out = out.finalize();
-
+fn write_alternatives<W: io::Write>(mut out: W, instance: &PackageInstance) -> io::Result<()> {
     let alternatives = match &instance.spec {
         PackageSpec::Service(spec) => &spec.alternatives,
         PackageSpec::ConfExt(spec) => &spec.alternatives,
@@ -29,6 +26,15 @@ pub fn generate(instance: &PackageInstance, out: LazyCreateBuilder) -> io::Resul
     if written {
         writeln!(out, "fi")?;
     }
+
+    Ok(())
+}
+
+pub fn generate(instance: &PackageInstance, out: LazyCreateBuilder) -> io::Result<()> {
+    let out = out.set_header("#!/bin/bash\n\nset -e\n\n");
+    let mut out = out.finalize();
+
+    write_alternatives(&mut out, instance)?;
 
     Ok(())
 }
