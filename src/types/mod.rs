91ffcca9a031a8b53b18550bf097906f4a68a341
Martin Habovstiak <martin.habovstiak@gmail.com>
2020-11-24 10:27:41 +0100

    Moved NonEmptyMap to its own module

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,31 +1,5 @@
-use std::borrow::Borrow;
-use crate::Map;
-
 pub use package_name::VPackageName;
+pub use non_empty_map::NonEmptyMap;
 
 mod package_name;
-
-pub struct NonEmptyMap<K, V, M: Borrow<Map<K, V>>>(M, std::marker::PhantomData<(K, V)>);
-
-#[derive(Debug, thiserror::Error)]
-#[error("The map is empty")]
-#[non_exhaustive]
-pub struct EmptyMapError;
-
-impl<K, V, M> NonEmptyMap<K, V, M> where M: Borrow<Map<K, V>> {
-    pub fn from_map(map: M) -> Option<Self> {
-        if map.borrow().is_empty() {
-            None
-        } else {
-            Some(NonEmptyMap(map, Default::default()))
-        }
-    }
-}
-
-impl<K, V, M> std::ops::Deref for NonEmptyMap<K, V, M> where M: Borrow<Map<K, V>> {
-    type Target = Map<K, V>;
-
-    fn deref(&self) -> &Self::Target {
-        self.0.borrow()
-    }
-}
+mod non_empty_map;
---
+++ src/types/non_empty_map.rs
@@ -0,0 +1,27 @@
+use std::borrow::Borrow;
+use crate::Map;
+
+pub struct NonEmptyMap<K, V, M: Borrow<Map<K, V>>>(M, std::marker::PhantomData<(K, V)>);
+
+#[derive(Debug, thiserror::Error)]
+#[error("The map is empty")]
+#[non_exhaustive]
+pub struct EmptyMapError;
+
+impl<K, V, M> NonEmptyMap<K, V, M> where M: Borrow<Map<K, V>> {
+    pub fn from_map(map: M) -> Option<Self> {
+        if map.borrow().is_empty() {
+            None
+        } else {
+            Some(NonEmptyMap(map, Default::default()))
+        }
+    }
+}
+
+impl<K, V, M> std::ops::Deref for NonEmptyMap<K, V, M> where M: Borrow<Map<K, V>> {
+    type Target = Map<K, V>;
+
+    fn deref(&self) -> &Self::Target {
+        self.0.borrow()
+    }
+}
